911ead87389bd340e558d178e2bcddeb4daaae39
antoniozandate <antonio.zandate@metricamovil.com>
2021-11-04 20:47:22 -0600

    updated todo location

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import { StyledEngineProvider } from "@mui/material/styles";
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { StyledEngineProvider } from '@mui/material/styles'
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import Todo from "./todo";
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import Todo from './components/todo/todo'
 
 ReactDOM.render(
-  <div style={{ height: "100vh" }}>
-    <StyledEngineProvider injectFirst>
-      <Todo />
-    </StyledEngineProvider>
-  </div>,
-  document.querySelector("#root")
-);
+	<div style={{ height: '100vh' }}>
+		<StyledEngineProvider injectFirst>
+			<Todo />
+		</StyledEngineProvider>
+	</div>,
+	document.querySelector('#root')
+)
